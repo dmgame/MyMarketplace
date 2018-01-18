60d04d8e2b5c4b744492def1ff7d3f2c39ade1ad
Ponezha Maria <ponezhamaria@gmail.com>
2018-01-18 22:08:21 +0200

    responsive

--- app/js/init.js
+++ app/js/init.js
@@ -1,15 +1,10 @@
 $(function () {
 
-  // init slick slider
-  $('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    autoplay: true,
-    arrows: false,
-    dots: true
+  // Open dropdown nav 
+  let nav = $('.dropdown-container');
+  nav.on('click', function(e) {
+  $(this).toggleClass('active');
   });
-
   // init form styler for select
   $('select').styler();
 
@@ -25,5 +20,89 @@ $(function () {
     appendTo: '.header .bottom-header .container',
     label: ''
   });
+
+  // Init slick slider with responsive
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
 
+// change project list
+(function ($){
+  $(window).on('load', function () {
+      var projectList = $('.tags');
+      var projectListActiveElement = projectList.find('.active');
+      var mobileProjectList = '<div class="mobile-list-wrap">' +
+          '<div class="mobile-list">' +
+          '<div class="current">' +
+          '<span class="current-text">' + projectListActiveElement.text() + '</span>' +
+          '<span class="dropdown__toggle">' +
+          '<i class="fa fa-close"></i>' +
+          '<i class="fa fa-chevron-down"></i>' +
+          '</span>' +
+          '</div>' +
+
+          '<div class="dropdown-list">'+
+          '<ul>'+
+          projectList.html() +
+          '</ul>' +
+          '</div>'+
+          '</div>'+
+          '</div>';
+
+      projectList.after(mobileProjectList);
+
+      var clickOnCurent = $('.current');
+      var currentText = $('.current-text');
+      var dropdownList = $('.dropdown-list');
+
+      clickOnCurent.on('click', function () {
+          $(this).toggleClass('selected');
+          dropdownList.slideToggle();
+      });
+
+      var dropdownListElement = $('.dropdown-list li');
+
+      dropdownListElement.on('click', function () {
+          dropdownListElement.each(function(i, value){
+              if($(this).hasClass('active')){
+                  $(this).removeClass('active');
+              }
+          });
+          $(this).addClass('active');
+          clickOnCurent.toggleClass('selected');
+          currentText.text($(this).text());
+          dropdownList.slideUp();
+          return false;
+      });
+  })
+})(jQuery);
+  
